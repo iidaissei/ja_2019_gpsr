b8b19f6bbe7c9b22d969f6a08b3372920ce446b1
iidaissei <issei.288.higashi@icloud.com>
2020-02-10 09:45:43 +0900

    2020/02/10

--- src/client_test.py
+++ src/client_test.py
@@ -5,36 +5,34 @@
 import sys
 #ROS関係ライブラリ
 import rospy
-from gpsr.srv import ActionPlan
-from ja_2019_gpsr.msg import *
+from mimi_common_pkg.msg import *
 import actionlib
 
-sys.path.insert(0, '/home/athome/catkin_ws/src/mimi_common_pkg/scripts/')
+sys.path.insert(0, '/home/issei/catkin_ws/src/mimi_common_pkg/scripts/')
 from common_action_client import *
 from common_function import *
 
-bc = BaseCarrier()
 
-class test():
+class ExeActionPlanAC():
     def __init__(self):
         self.ac = actionlib.SimpleActionClient('exe_action_plan', ExeActionPlanAction)
-        speak('start action')
-        self.lsten_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        self.goal = ExeActionPlanGoal()
+
+    def execute(self, action_list, data_list):
         self.ac.wait_for_server()
-        goal = ExeActionPlanGoal()
-        speak('Please give me a order')
-        action_plan = self.lsten_srv()
-        goal.action = action_plan.action
-        goal.data = action_plan.data
-        self.ac.send_goal(goal)
+        self.goal.action = action_list
+        self.goal.data = data_list
+        self.ac.send_goal(self.goal)
         self.ac.wait_for_result()
         result = self.ac.get_result()
         if result.data == 'success':
-            rospy.loginfo("Success EnterTheRoom")
+            rospy.loginfo("Success ExeActionPlan")
+            rospy.sleep(0.3)
             self.ac.cancel_goal()
             return True
         else:
-            rospy.loginfo("Failed EnterTheRoom")
+            rospy.loginfo("Failed ExeActionPlan")
+            rospy.sleep(0.3)
             self.ac.cancel_goal()
             return False
 
